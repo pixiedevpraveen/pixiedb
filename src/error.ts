9369809c19cdde0080c861099fdabf7f27c8aec5
Praveen Yadav <praveenyadav7787@gmail.com>
2024-03-20 22:06:59 +0530

    add PDBError class to throw custom error in error.ts

---
+++ src/error.ts
@@ -0,0 +1,9 @@
+export class PDBError<N extends PDBErrorType> extends Error {
+    name: `${PDBErrorType}Error`
+    constructor(name: N, msg?: string) {
+        super(msg)
+        this.name = `${name}Error`
+    }
+}
+
+type PDBErrorType = "Property" | "Value" | "Type" | "Event" | "Action" | "Filter"
