a2d0f2fd7bad5b5e3497b102bbe64b562088823b
Praveen Yadav <praveenyadav7787@gmail.com>
2024-03-20 22:13:56 +0530

    add state and method in PixieDb add ResultSet class for filtering data

---
+++ src/ResultSet.ts
@@ -0,0 +1,493 @@
+import { PDBError } from "./error"
+import { PixieDb } from "./index"
+import type { FQ, Index, MaybePartial, Pretify, SortOptions, OrderBy } from "./types"
+import { deepClone, hasOwn, isArray, isNullOrUndefined } from "./utils"
+
+export class ResultSet<T extends Record<any, any>, Fields extends readonly (keyof T)[] | [], Key extends keyof T> {
+    /**
+     * fields Queries to filter rows
+    */
+    private fieldsQuery: FQ<T, keyof T>[] = []
+    /**
+     * Filtered rows
+    */
+    private filteredrows: T[] = []
+    /**
+     * Filtered status
+    */
+    private filtered = false
+    /**
+     * Sorted status
+    */
+    private sorted = false
+    /**
+     * From index
+    */
+    private from = 0
+    /**
+     * Limit index
+    */
+    private max = -1
+
+    /**
+     * Clone status
+    */
+    private clone = true
+
+    /**
+      * Action to perform "select" | "where"
+      * @type {"select" | "where"}
+     */
+    private action: "select" | "where"
+
+    /**
+     * primary key or unique key for the database
+    */
+    private key: Key
+
+    /**
+     * keyMap is a map of key and data
+    */
+    private kmp
+
+    /**
+      * indexes is a map of index name and map of index value and set of keys
+    */
+    private idx
+
+    /**
+     * fields to return mapped object with select
+    */
+    private readonly fields: readonly (keyof T)[]
+
+    private sortOptions: OrderBy<T>[] = []
+    // /**
+    //  * F: "filter"
+    //  * S: "sort"
+    //  * O: "offset"
+    //  * L: "limit"
+    // */
+    private opStack: ("F" | "S" | "O" | "L")[] = []
+
+    /**
+     * @param db PixieDb instance
+    */
+    private readonly db: PixieDb<T, Key>
+
+    constructor(db: PixieDb<T, Key>, key: Key, kmp: Map<T[Key], T>, idx: Index<T, Key>, fields?: Fields, action: "select" | "where" = "select") {
+        this.db = db
+        this.key = key
+        this.kmp = kmp
+        this.idx = idx
+        this.action = action
+        this.fields = fields ?? []
+    }
+
+    /**
+     * Get data by index
+     * @param name index name
+     * @param value index value
+    */
+    private getByIndex<K extends keyof T>(name: K, value: T[K]): T[] {
+        const res: T[] = []
+        const ks = this.idx[name]?.get(value)
+
+        if (ks)
+            ks.forEach(k => {
+                const v = this.kmp.get(k)
+                if (v) res.push(v)
+            })
+        return res
+    }
+
+    /**
+     * Used to clone document
+    */
+    private clonedDoc(doc: T, force?: boolean) {
+        if (!this.clone && !force) return doc
+
+        return deepClone(doc)
+    }
+
+    /**
+     * Pick fields from document
+    */
+    private pick(doc: T) {
+        if (!this.fields.length) return doc
+        const data = {} as T;
+        this.fields.forEach(k => data[k] = doc[k])
+        return data
+    }
+
+    /**
+     * Filter data using fieldsQuery
+     * if data already filtered then return
+     * if fieldsQuery is empty then return all data
+     * if fieldsQuery is not empty then filter data and return
+     * if fieldsQuery is not empty and data is already filtered then filter data and return
+     * also perform limit and from/offset action
+    */
+    private filter() {
+        if (this.filtered) return
+        let started = false
+        // console.log(this.from, this.max);
+        const fq = this.fieldsQuery[0];
+        if (!fq) {
+            if (this.from === 0 && this.max === 1)
+                this.filteredrows = [this.kmp.values().next().value]
+            else
+                this.setSliced()
+            this.filtered = true
+            return
+        }
+
+        if (fq[0] === "eq") {
+
+            if (fq[1] === this.key) {
+                const v = this.kmp.get(fq[2] as T[Key])
+                if (v)
+                    this.filteredrows.push(v)
+
+                started = true
+            } else if (hasOwn(this.idx, fq[1])) {
+                const vs = this.getByIndex(fq[1], fq[2])
+                this.filteredrows.push(...vs)
+                started = true
+            }
+        }
+
+        else if (fq[0] === "in") {
+            if (fq[1] === this.key) {
+                fq[2].forEach(val => {
+                    const v = this.kmp.get(val as T[Key])
+                    if (v)
+                        this.filteredrows.push(v)
+                })
+                started = true
+            } else if (hasOwn(this.idx, fq[1])) {
+                fq[2].forEach(val => {
+                    const vs = this.getByIndex(fq[1], val)
+                    this.filteredrows.push(...vs)
+                })
+                started = true
+            }
+        }
+        if (started)
+            this.fieldsQuery.shift()
+
+        if (!this.fieldsQuery.length) {
+            if (!started)
+                this.filteredrows = this.setSliced()
+            this.filtered = true
+            return
+        }
+
+
+
+        const data = started ? this.filteredrows : this.kmp.values()
+
+        this.filteredrows = []
+
+        for (const d of data) {
+            const add = !this.fieldsQuery.some(([op, field, val1, val2]) => {
+                // return opposite
+                switch (op) {
+                    case "eq":
+                        return d[field] !== val1
+                    case "gt":
+                        return d[field] <= val1
+                    case "in":
+                        return !val1.has(d[field])
+                    case "lt":
+                        return d[field] >= val1
+                    case "neq":
+                        return d[field] === val1
+                    case "lte":
+                        return d[field] > val1
+                    case "gte":
+                        return d[field] < val1
+                    case "nin":
+                        return val1.has(d[field])
+                    case "btw":
+                        return !(d[field] >= val1 && d[field] <= val2)
+                    case "nbtw":
+                        return d[field] >= val1 && d[field] <= val2
+                    default:
+                        throw new PDBError("Filter", "Unsupported filter: " + op)
+                }
+            })
+            if (add && --this.from < 0) {
+                this.filteredrows.push(d)
+                if (this.max !== -1) {
+                    if (--this.max <= 0) break
+                }
+            }
+        }
+        this.filtered = true
+        this.from = 0
+        this.max = -1
+    }
+
+    /**
+     * perform update delete task here to reduce bundle size or in filter method
+    */
+    private performUD(docs: T[], data?: MaybePartial<T>) {
+        this.validateAction("where")
+
+        const r: T[] = []
+        let st: Set<any> | undefined
+        const ix = this.idx
+        let ikeys = Object.keys(ix) as (keyof typeof data)[]
+
+        if (data)
+            ikeys = ikeys.filter(i => hasOwn(data, i))
+
+        docs.forEach(d => {
+            ikeys.forEach(i => {
+
+                st = ix[i].get(d[i])
+                if (st) st.delete(d.id)
+
+                st = ix[i].get(d[i])
+                if (st) {
+                    st.delete(d[this.key])
+                    if (!st.size)
+                        ix[i].delete(d[i])
+                }
+
+                if (data) {
+                    st = ix[i].get(data[i])
+                    if (!st) {
+                        st = new Set();
+                        ix[i].set(data[i], st);
+                    }
+                    st.add(d[this.key])
+                } else {
+                    this.kmp.delete(d[this.key])
+                    this.db.emit("D", d)
+                }
+            })
+
+            let c = d
+            if (data) {
+                Object.assign(d, data)
+                const o = this.clonedDoc(c)
+                c = this.clonedDoc(d, true)
+                this.db.emit("U", c, o)
+            }
+
+            r.push(c)
+        })
+        return r
+    }
+
+    /**
+     * Delete filtered data
+    */
+    delete(): T[] {
+        this.validateAction("where")
+        this.clone = false
+        this.filter()
+        return this.performUD(this.filteredrows)
+    }
+
+    /**
+     * Update filtered data
+     * @param doc document to update
+    */
+    update(doc: T | Partial<T>): T[] {
+        if (hasOwn(doc, this.key)) delete doc[this.key]
+
+        this.validateAction("where")
+        this.filter()
+        return this.performUD(this.filteredrows, doc)
+    }
+
+    /**
+     * Returns filtered list of shallow copied doc
+    */
+    data() {
+        this.validateAction("select")
+        this.filter()
+        // console.log(this.from, this.max);
+        this.setSliced(this.filteredrows)
+        return this.filteredrows.map(r => this.clonedDoc(this.pick(r))) as Fields extends [] ? T[] : Pick<Pretify<T>, Fields[number]>[]
+    }
+
+    /**
+     * Returns filtered single shallow copied doc
+    */
+    single() {
+        this.validateAction("select")
+        this.filter()
+        this.setSliced(this.filteredrows)
+        return this.clonedDoc(this.pick(this.filteredrows[0])) as Fields extends [] ? T : Pick<Pretify<T>, Fields[number]>
+    }
+
+    /**
+     * @throws error if action don't match
+    */
+    private validateAction(act: "select" | "where"): void {
+        if (this.action !== act) throw new PDBError("Action", "unable to perform this action in " + act)
+    }
+
+    /**
+     * Set from/offset index and limit index
+    */
+    range(from = 0, count = -1) {
+        // TODO: throw error for from < 0
+        if (from >= 0)
+            this.from = from
+        if (count >= 0)
+            this.max = count
+        this.opStack.push("O")
+        return this
+    }
+
+    private setSliced(data: T[] = [...this.kmp.values()]): T[] {
+        this.filteredrows = this.from !== 0 || this.max !== -1 ? data.slice(this.from, this.max === -1 ? undefined : this.from + this.max) : data
+        this.from = 0
+        this.max = -1
+        return this.filteredrows
+    }
+
+    /**
+     * Count of the filted data
+    */
+    count(): number {
+        this.clone = false
+        if (!this.fieldsQuery.length) return this.kmp.size
+        this.filter()
+        return this.filteredrows.length
+    }
+
+    /**
+     * Sort data using keys
+    */
+    orderBy(opt: SortOptions<T>) {
+        this.sortOptions = opt.map<OrderBy<T>>(o => isArray(o) && o.length === 2 ? o : [o, "asc"])
+        this.opStack.push("S")
+        this.filter()
+        this.sort()
+        return this
+    }
+
+    /**
+     * Sort data using keys
+     * @param opt sort options
+    */
+    private sort() {
+        if (!this.sortOptions.length) return
+        // if (this.sorted) throw new PDBError("Action", "Already sorted")
+        this.validateAction("select")
+        let len = this.sortOptions.length
+
+        this.filteredrows.sort((a, b) => {
+            let comparison = 0;
+
+            for (let i = 0; i < len; i++) {
+                const op = this.sortOptions[i]
+                const priority = len - i
+
+                // [key: keyof T, order: "asc" | "desc"]
+                const [key, order] = op
+
+                const value1 = isNullOrUndefined(a[key]) ? 0 : a[key];
+                const value2 = isNullOrUndefined(b[key]) ? 0 : b[key];
+
+                let cmp = value1 === value2 ? 0 : (
+                    value1 < value2 ? -1 : 1
+                )
+                cmp *= priority;
+                if (order === "desc") cmp *= -1
+
+                comparison += cmp
+
+            }
+            return comparison;
+        });
+        this.sorted = true
+    }
+
+    eq<K extends keyof T>(field: K, value: T[K]) {
+        return this.fAdd("eq", field, value)
+    }
+
+    in<K extends keyof T>(field: K, values: T[K][]) {
+        if (!isArray(values)) throw new PDBError("Value", "Invalid values")
+        const st = new Set<any>(values)
+        return this.fAdd("in", field, st)
+    }
+    nin<K extends keyof T>(field: K, values: T[K][]) {
+        if (!isArray(values)) throw new PDBError("Value", "Invalid values")
+        const st = new Set<any>(values)
+        return this.fAdd("nin", field, st)
+    }
+
+    between<K extends keyof T>(field: K, values: [T[K], T[K]]) {
+        if (!isArray(values) || values.length !== 2) throw new PDBError("Value", "Invalid values")
+
+        return this.fAdd("btw", field, values[0], values[1])
+    }
+
+    nbetween<K extends keyof T>(field: K, values: [T[K], T[K]]) {
+        if (!isArray(values) || values.length !== 2) throw new PDBError("Value", "Invalid values")
+
+        return this.fAdd("nbtw", field, values[0], values[1])
+    }
+
+    neq<K extends keyof T>(field: K, value: T[K]) {
+        return this.fAdd("neq", field, value)
+    }
+
+    gt<K extends keyof T>(field: K, value: T[K]) {
+        return this.fAdd("gt", field, value)
+    }
+
+    gte<K extends keyof T>(field: K, value: T[K]) {
+        return this.fAdd("gte", field, value)
+    }
+
+    lt<K extends keyof T>(field: K, value: T[K]) {
+        return this.fAdd("lt", field, value)
+    }
+
+    lte<K extends keyof T>(field: K, value: T[K]) {
+        return this.fAdd("lte", field, value)
+    }
+
+    /**
+     * push action field and values to fieldsQuery
+    */
+    private fAdd(...v: FQ<T, keyof T>) {
+        // if (this.opStack.length < 1)
+        // this.opAdd("F")
+        const prev = this.opStack.at(-1)
+        if (prev && prev !== "F") throw new PDBError("Action", "Unable to add filter")
+        // if (this.sorted) throw new PDBError("Action", "Unable to add filter after sort")
+        this.fieldsQuery.push(v)
+        return this
+    }
+    private opAdd(op: typeof this.opStack[number]) {
+        const prev = this.opStack.at(-1)
+
+        if (!prev) {
+            this.opStack.push(op)
+            return
+        }
+        switch (op) {
+            case "F":
+                if (prev !== "F") throw new PDBError("Action", "Unable to add filter")
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    toJSON(clone = false) {
+        this.validateAction("select")
+        this.clone = clone
+        return this.data()
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import { EvEmit } from "./EvEmit";
+import { ResultSet } from "./ResultSet";
+import { PDBError } from "./error";
+import { deepClone, freeze, hasOwn, isArray, toArray } from "./utils";
+import type { Index, SelectQueryBuilder, WhereQueryBuilder } from "./types";
+
 /**-----------------------------------------------------
  * A tiny javascript in memory database with indexing and filters.
  *
@@ -10,5 +16,168 @@
  * const allByName = pd.select().eq("name", "Apple").orderBy(["name", ["price", "desc"]]).data() // [{ id: 1, name: "Apple", price: 10, category: "Fruit" }, ...]
  * console.log(byId, allByName);
  */
-export class PixieDb<T extends Record<any, any>, Key extends keyof T> {
+export class PixieDb<T extends Record<any, any>, Key extends keyof T> extends EvEmit<T> {
+    /**
+     * primary key or unique key for the database
+    */
+    readonly key
+
+    /**
+     * keyMap is a map of key and data
+    */
+    private keyMap: Map<T[Key], T> = freeze(new Map<T[Key], T>())
+
+    /**
+     * indexes is a map of index name and map of index value and set of keys
+    */
+    private indexes: Index<T, Key>
+
+    /**
+     * @param key primary key or unique key for the database
+     * @param indexes list of index names
+     * @param data data list/array to load (with clone)
+    */
+    constructor(key: Key, indexes: readonly (keyof T)[] = [], data?: T[]) {
+        super(200)
+        this.key = key
+
+        let idx = Object.create(null) as Index<T, Key>
+        indexes.forEach(i => idx[i] instanceof Map || (idx[i] = new Map()))
+        this.indexes = freeze(idx)
+        if (data) this.load(data)
+    }
+
+    /**
+     * used to perform select after complex filtering
+     * @example
+     * pd.select(["name", "price"]).eq("category", "Fruit").data() // [{ name: "Apple", price: 10 }, { name: "Banana", price: 10 }, ...]
+     * pd.select().eq("category", "Fruit").orderBy("name", ["price", "desc"]).data() // [{ id: 1, name: "Apple", price: 10, category: "Fruit" }, { id: 2, name: "Banana", price: 10, category: "Fruit" }, ...] 
+    */
+    select<Fields extends readonly (keyof T)[]>(fields?: Fields) {
+        return new ResultSet<T, Fields, Key>(this, this.key, this.keyMap, this.indexes, fields) as unknown as SelectQueryBuilder<T, Fields>
+    }
+
+    /**
+     * used to perform delete/update with complex filtering
+     * @example
+     * pd.where().eq("category", "Fruit").delete() // delete all fruit products
+     * pd.where().eq("category", "Fruit").update({ price: 20 }) // update all fruit products price to 20
+    */
+    where() {
+        return new ResultSet<T, [], Key>(this, this.key, this.keyMap, this.indexes, [], "where") as unknown as WhereQueryBuilder<T>
+    }
+
+    /**
+     * Insert data or array of data into database
+     * @example
+     * pd.insert({ id: 3, name: "Orange", price: 20, category: "Fruit" }) // insert single record
+     * pd.insert([{ id: 3, name: "Orange", price: 20, category: "Fruit" }, { id: 4, name: "Mango", price: 30, category: "Fruit" }]) // insert multiple records
+     * @param docs data or array of data to insert
+     * @param upsert set true to update if found
+     * @param silent true to not emit events default false
+     * @param clone false to not clone data before insert
+    */
+    insert<Doc extends T | T[]>(docs: Doc, { silent, clone, upsert }: { silent?: boolean, clone?: boolean, upsert?: boolean } = {}) {
+        if (typeof docs !== 'object') throw new PDBError("Value", "Value must be an object or object array")
+        const ds = toArray(docs)
+        let st: Set<any> | undefined
+        const r: T[] = []
+        const ix = this.indexes
+        const key = this.key
+        const keys = this.indexNames
+
+        ds.forEach(d => {
+            try {
+                if (this.keyMap.get(d[key])) {
+                    if (upsert) {
+                        const u = this.where().eq(this.key, d[this.key]).update(d)
+                        if (u.length)
+                            r.push(d)
+                    }
+                    return
+                }
+
+                keys.forEach(i => {
+                    if (!hasOwn(ix, i)) return
+
+                    st = ix[i as keyof T].get(d[i as keyof T])
+                    if (!st) {
+                        st = new Set();
+                        ix[i as keyof T].set(d[i as keyof T], st);
+                    }
+                    st.add(d[key])
+                })
+                this.keyMap.set(d[key], clone === false ? d : deepClone(d))
+                if (!silent) this.emit("I", d)
+                r.push(d)
+
+            } catch (er) {
+            }
+        })
+
+        return (isArray(docs) ? r : r[0]) as Doc extends any[] ? T[] : (T | undefined)
+    }
+
+    /**
+     * @param data data list/array to load (without clone)
+     * @param clear true to clear existing data
+    */
+    load(data: T[], clear = false) {
+        if (clear) {
+            this.keyMap.clear()
+            Object.values(this.indexes).forEach(i => i.clear())
+        }
+
+        this.insert(data, { silent: true, clone: false })
+        this.emit("L")
+    }
+
+    /**
+     * get doc using key (primary key/unique id)
+     * @example
+     * const getByKey = pd.get(2)  // { id: 2, name: "Banana", price: 10, category: "Fruit" }
+     * 
+     * @param {T[Key]} key key to get data by
+     */
+    get(key: T[Key]): T | undefined {
+        return this.keyMap.get(key)
+    }
+
+    /**
+     * @param clone
+     * clone the returning rows (don't modify values if pass clone as false)
+     * default `true`
+     * @example
+     * const allData = pd.data() // [{ id: 1, name: "Apple", price: 10, category: "Fruit" }, ...]
+    */
+    data(clone = true): T[] {
+        const d = [...this.keyMap.values()]
+        return clone ? d.map(i => deepClone(i)) : d
+    }
+
+    get indexNames(): (keyof T)[] {
+        return Object.keys(this.indexes)
+    }
+
+    /**
+     * to close/quit/terminate the database
+     * @param silent true to not emit events default false
+    */
+    close(silent = false): void {
+        this.keyMap.clear()
+        Object.keys(this.indexes).forEach(i => {
+            if (hasOwn(this.indexes, i)) this.indexes[i].clear()
+        })
+        if (!silent)
+            this.emit("Q")
+    }
+
+    /**
+     * return JSON of all data without cloning, key and index names
+     * @example
+     * const json = pd.toJSON() // { key: "id", indexes: ["price", "category"], data: [{ id: 1, name: "Apple", price: 10, category: "Fruit" }, ...] }
+    */
+    toJSON() {
+        return { key: this.key, indexes: this.indexNames, data: this.data() }
+    }
 }
